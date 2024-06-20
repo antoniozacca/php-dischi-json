e186e4da258e8ba0b4b79518bf40147e3219174d
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-20 17:05:30 +0200

    complete exercise with bootstrap and style.css

--- index.php
+++ index.php
@@ -5,17 +5,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Dischi JSON</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./style.css">
   <script src="https://cdn.jsdelivr.net/npm/axios@1.6.1/dist/axios.min.js"></script>
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
-  <div id="app">
-    <div v-for="todo in todos" :key="todo.title">
-      <img :src="todo.img" >
-      <div class="card-body">
-        <h2 class="card-title">{{ todo.title }}</h2>
-        <p class="card-text">{{ todo.artist }}</p>
-        <p class="card-text">{{ todo.year }}</p>
+  <!-- Header -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-header">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg">
+      </a>
+    </div>
+  </nav>
+
+  <!-- Main Content -->
+  <div id="app" class="container mt-5 mb-5">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+      <div v-for="todo in todos" :key="todo.title" class="col">
+        <div class="card h-100">
+          <img :src="todo.img" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">{{ todo.title }}</h5>
+            <p class="card-text">{{ todo.artist }}</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">{{ todo.year }}</small>
+          </div>
+        </div>
       </div>
     </div>
   </div>
