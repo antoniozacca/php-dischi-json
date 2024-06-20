c7e98d4616d0a4493189050912e04b8a82fe79cf
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-20 16:01:43 +0200

    fix axios

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Dischi JSON</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/axios@1.6.7/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
     <div id="app">
         <h1>ciao</h1>
-        <script src="https://unpkg.com/vue@3/dist/vue.global.prod.js"></script>
-        <script src="./main.js"></script>
     </div>
+    <script src="./main.js"></script>
 </body>
 </html>
