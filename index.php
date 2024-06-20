388caab360a6fecf66626836fb7937552ecc26e6
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-20 15:08:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+$todos = [
+    [
+        'img' => '',
+        'title' => 'New Jersey',
+        'artist' => 'Bon Jovi',
+        'year' => '1988'
+    ],
+    [
+        'img' => '',
+        'title' => 'Live at Wembley 86',
+        'artist' => 'Queen',
+        'year' => '1992'
+    ],
+    [
+        'img' => '',
+        'title' => 'Ten"s Summoner"s Tales',
+        'artist' => 'Sting',
+        'year' => '1993'
+    ],
+    [
+        'img' => '',
+        'title' => 'Steve Gadd band',
+        'artist' => 'Steve Gadd band',
+        'year' => '2018'
+    ],
+    [
+        'img' => '',
+        'title' => 'Brave new World',
+        'artist' => 'Iron Maiden',
+        'year' => '2000'
+    ],
+    [
+        'img' => '',
+        'title' => 'One more car, one more rider',
+        'artist' => 'Eric Clapton',
+        'year' => '2002'
+    ],
+];
